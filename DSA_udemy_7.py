bb1bd80bc3219e06788e8e70ac6c2c5cabc4e420
slaterc3 <cslat22@my.wgu.edu>
2021-11-19 07:19:21 -0500

    Initial commit

---
+++ DSA_udemy_7.py
@@ -0,0 +1,15 @@
+# def print_items(n):
+#     for i in range(n):
+#         for j in range(n):
+#             for k in range(n):
+#                 print(i,j,k)
+#
+# print_items(3)
+my_list = [11, 3, 23, 7]
+my_list.append(17)
+my_list.pop()
+my_list.pop(0)
+my_list.insert(0, 11)
+
+print(my_list)
+
---
+++ queues.py
@@ -0,0 +1,55 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+
+class Queue:
+    def __init__(self, value):
+        new_node = Node(value)
+        self.first = new_node
+        self.last = new_node
+        self.length = 1
+
+    def enqueue(self, value):
+        new_node = Node(value)
+        if self.length == 0:
+            self.first = new_node
+            self.last = new_node
+        else:
+            self.last.next = new_node
+            self.last = new_node
+        self.length += 1
+        # return True
+
+    def dequeue(self):
+        if self.length == 0:
+            return None
+        temp = self.first
+        if self.length == 1:
+            self.first = None
+            self.last = None
+        else:
+            self.first = self.first.next
+            temp.next = None
+        self.length -= 1
+        return temp
+
+    def print_queue(self):
+        temp = self.first
+        while temp:
+            print(temp.value)
+            temp = temp.next
+
+
+my_queue = Queue(1)
+my_queue.print_queue()
+print()
+my_queue.enqueue(2)
+my_queue.enqueue(3)
+my_queue.enqueue(4)
+my_queue.print_queue()
+print()
+
+
+my_queue.print_queue()
